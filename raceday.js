7980555597d55430f9eeeca8d561767f30279acb
Dan Mateer <dan@pullrequest.com>
2018-02-12 15:52:06 -0600

    Expert

--- raceday.js
+++ raceday.js
@@ -1,6 +1,7 @@
 let raceNumber = Math.floor(Math.random()*1000);
 let registeredEarly = false;
 let runnerAge = 35;
+let runnerSkill = 'Expert';
 
 if (registeredEarly !== true) {
   raceNumber += 1000;
@@ -12,6 +13,8 @@ if ((runnerAge >= 18) && (registeredEarly == true)) {
   console.log(`Your race number is ${raceNumber}. Your race start time is 11:00am.`);
 } else if ((runnerAge < 18) && (registeredEarly == false)) {
   console.log(`Your race number is ${raceNumber}. Your race start time is 12:30pm.`);
+} else if (runnerSkill == 'Expert') {
+  console.log('Go to the special starting gate at 9:00am.')
 } else {
   console.log('Please see the registration desk.');
 }
