fcdc295e8f42bfed3fc82f9e0b7b97c80b13c20c
Dan Mateer <dan@pullrequest.com>
2018-02-12 15:03:13 -0600

    Adding raceday file

---
+++ raceday.js
@@ -0,0 +1,17 @@
+let raceNumber = Math.floor(Math.random()*1000);
+let registeredEarly = false;
+let runnerAge = 35;
+
+if (registeredEarly !== true) {
+  raceNumber += 1000;
+};
+
+if ((runnerAge >= 18) && (registeredEarly == true)) {
+  console.log(`Your race number is ${raceNumber}. Your race start time is 9:30 am.`);
+} else if ((runnerAge >= 18) || (!registeredEarly == true)) {
+  console.log(`Your race number is ${raceNumber}. Your race start time is 11:00 am.`);
+} else if ((runnerAge < 18) && (registeredEarly == false)) {
+  console.log(`Your race number is ${raceNumber}. Your race start time is 12:30 pm.`);
+} else {
+  console.log('Please see the registration desk.');
+}
